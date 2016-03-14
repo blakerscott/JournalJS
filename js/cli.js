577b21e5a8c13a845b31f81fd5d2ecc685fd9a3e
Blake Scott and Erik Tolentino <tolentino3575@gmail.com>
2016-03-14 11:30:58 -0700

    fix bug in cli

--- js/cli.js
+++ js/cli.js
@@ -2,7 +2,7 @@ var titleLength = require('./titleLength.js').titleLength;
 var prompt = require('prompt');
 prompt.start();
 
-prompt.git('title', function(err, result) {
+prompt.get('title', function(err, result) {
   console.log('Length of title!');
   var result = titleLength(result.title);
   console.log(result);
