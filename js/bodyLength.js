884bab3cb26b77c01b5331bfb59d85741cacbbec
Blake Scott and Erik Tolentino <blakerscott@gmail.com>
2016-03-14 14:56:53 -0700

    removed titleLength and bodyLength files

--- js/bodyLength.js
+++
@@ -1,8 +0,0 @@
-//Length method for number of words in the title
-exports.bodyLength = function(body) {
-  var bodyArray = body.split(" ");
-  var bodyLength = bodyArray.length;
-
-  return bodyLength;
-
-}
